de1889a69531878c99e66c16617a5ffcfc2c9cab
lrbertti <lrbertti@gmail.com>
2021-07-31 16:40:37 -0300

    primeiro commit

---
+++ DigitalInnovationOneExemplo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DigitalInnovationOneExemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
